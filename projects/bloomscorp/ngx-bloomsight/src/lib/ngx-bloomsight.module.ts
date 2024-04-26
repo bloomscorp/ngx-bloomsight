8e7d260370c9e1097895b9d94da4f99467d0103c
Deep-Dey <deepdey20998@gmail.com>
2024-04-26 16:10:39 +0530

    chore: update providers

--- projects/bloomscorp/ngx-bloomsight/src/lib/ngx-bloomsight.module.ts
+++ projects/bloomscorp/ngx-bloomsight/src/lib/ngx-bloomsight.module.ts
@@ -2,6 +2,7 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {NgxBloomsightComponent} from './ngx-bloomsight.component';
 import {BloomsightEventService} from './service/bloomsight-event.service';
 import {IConfig} from '@bloomscorp/bloomsight.js/dist/configuration/interface/config';
+import {BloomsightMailService} from '@bloomscorp/ngx-bloomsight';
 
 
 
@@ -21,7 +22,11 @@ export class NgxBloomsightModule {
 
         return {
             ngModule: NgxBloomsightModule,
-            providers: [BloomsightEventService, {provide: 'config', useValue: config}]
+            providers: [
+                BloomsightEventService, 
+                BloomsightMailService, 
+                {provide: 'config', useValue: config}
+            ]
         };
     }
 }
